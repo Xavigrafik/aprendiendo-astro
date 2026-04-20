bba5cea833092ba5f76957e908119810485b56e5
xavi <xavigrafik@gmail.com>
2026-04-20 13:54:45 +0200

    layouts, json

--- projects/00-portfolio/src/content.config.ts
+++ projects/00-portfolio/src/content.config.ts
@@ -1,15 +1,30 @@
 // src/content.config.ts
 import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders'; 
+import { glob, file } from 'astro/loaders'; 
 
+// PROJECTS ======================================
 const projects = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/projects" }),
-  schema: ({ image }) => z.object({
-    title: z.string(),
-    description: z.string(),
-    tags: z.array(z.string()),
-    image: image().optional(),
-  }),
+
+    loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/projects" }),
+
+    schema: ({ image }) => z.object({
+        title: z.string(),
+        description: z.string(),
+        tags: z.array(z.string()),
+        image: image().optional(),
+    }),
 });
 
-export const collections = { projects };
+// BOOKS ======================================
+const books = defineCollection({
+    loader: file("src/content/data/books.json"),
+    schema: z.object({
+        id: z.string(),
+        title: z.string(),
+        author: z.string(),
+        url: z.string().url(),
+        description: z.string(),
+    }),
+});
+
+export const collections = { projects, books };
